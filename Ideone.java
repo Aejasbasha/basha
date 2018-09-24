8d3308fd690470bc7f7758e6c424026ae987fa5e
Aejasbasha <43529647+Aejasbasha@users.noreply.github.com>
2018-09-24 10:43:00 +0530

    Create Ideone.java

---
+++ Ideone.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        if(n > 0)
+        {
+            System.out.println("Positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println("Negative");
+        }
+	}
+}
